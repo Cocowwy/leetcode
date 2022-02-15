06fcf8c798fed8194ce7bd7e6946958168c87e97
Cocowwy <514658459@qq.com>
2022-02-15 10:41:10 +0800

    T1115 JUC 交替打印

---
+++ src/juc/T1115.java
@@ -0,0 +1,49 @@
+package juc;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * 交替打印 FooBar
+ * https://leetcode-cn.com/problems/print-foobar-alternately/
+ *
+ * 思路 1：用两个信号量，打印完通知另外一个线程的信号量
+ *
+ *
+ * @author cocowwy.cn
+ * @create 2022-02-02-10:20
+ */
+public class T1115 {
+
+}
+
+class FooBar {
+    private int n;
+    private Semaphore foo = new Semaphore(1);
+    private Semaphore bar = new Semaphore(0);
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            foo.acquire();
+            printFoo.run();
+            bar.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            bar.acquire();
+            printBar.run();
+            foo.release();
+        }
+    }
+}
