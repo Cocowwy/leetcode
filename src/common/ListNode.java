da7192c77c2ee517a9502893d680ae159d42e4e8
Cocowwy <514658459@qq.com>
2022-05-15 12:15:22 +0800

    ListNode

---
+++ src/common/ListNode.java
@@ -0,0 +1,22 @@
+package common;
+
+/**
+ * @author Cocowwy
+ * @create 2022-05-05-11:27
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
